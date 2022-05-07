839aea13154c699fed2e999db76a98c8acea572d
OscarArr <oscar.arrhenius@gmail.com>
2022-05-07 14:31:23 +0200

    fixed endpoints category and category recipe search

--- server/src/routes/category.ts
+++ server/src/routes/category.ts
@@ -14,19 +14,33 @@ router.get("/", async (req, res) => {
   }
 })
 
+// router.get("/:category", async (req, res) => {
+//   if(Object.keys(req.params).includes("category")){
+//     const recipes = await getRecipesByCategory(req.params.category)
+//     res.json(recipes)
+//   }
+// })
+
 router.get("/:category", async (req, res) => {
-  if(Object.keys(req.params).includes("category")){
+  const query = req.query
+  if(Object.keys(req.query).length > 0){
+    if(Object.keys(req.query).includes("search")){
+      console.log(query)
+      const recipes = await getRecipesByCategoryAndQuery(req.params.category, query.search)
+      res.json(recipes)
+    }
+  } else if(Object.keys(req.params).includes("category")){
     const recipes = await getRecipesByCategory(req.params.category)
     res.json(recipes)
   }
 })
 
-router.get("/:category/:searchQuery", async (req, res) => {
-  if(Object.keys(req.params).includes("category")){
-    const recipes = await getRecipesByCategoryAndQuery(req.params.category, req.params.searchQuery)
-    res.json(recipes)
-  }
-})
+// router.get("/:category/:searchQuery", async (req, res) => {
+//   if(Object.keys(req.params).includes("category")){
+//     const recipes = await getRecipesByCategoryAndQuery(req.params.category, req.params.searchQuery)
+//     res.json(recipes)
+//   }
+// })
 
 
 export default router
